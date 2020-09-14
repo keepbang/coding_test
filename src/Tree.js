0238b6eec6e37b4b311feb905e18536256e268d8
keepbang <rlqud1125@gmail.com>
2020-09-14 22:17:34 +0900

    tree search

--- src/Tree.js
+++ src/Tree.js
@@ -1,83 +1,102 @@
 function TreeNode(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
-  
-    TreeNode.prototype.addNode = function (val) {
-      if (this.value > val) {
-        if (this.left === null) {
-          this.left = new TreeNode(val);
-        } else {
-          this.left.addNode(val);
-        }
-      } else if (this.value < val) {
-        if (this.right === null) {
-          this.right = new TreeNode(val);
-        } else {
-          this.right.addNode(val);
-        }
+  this.value = value;
+  this.left = null;
+  this.right = null;
+
+  TreeNode.prototype.addNode = function (val) {
+    if (this.value > val) {
+      if (this.left === null) {
+        this.left = new TreeNode(val);
+      } else {
+        this.left.addNode(val);
       }
-    };
-  
-    TreeNode.prototype.getValue = function () {
-      return this.value;
-    };
+    } else if (this.value < val) {
+      if (this.right === null) {
+        this.right = new TreeNode(val);
+      } else {
+        this.right.addNode(val);
+      }
+    }
+  };
+
+  TreeNode.prototype.getValue = function () {
+    return this.value;
+  };
+}
+
+function preOrder(node) {
+  let result = [];
+  result.push(node.value);
+  if (node.left !== null) {
+    result = result.concat(preOrder(node.left));
   }
-  
-  function preOrder(node) {
-    let result = [];
-    result.push(node.value);
-    if (node.left !== null) {
-      result = result.concat(preOrder(node.left));
-    }
-    if (node.right !== null) {
-      result = result.concat(preOrder(node.right));
-    }
-  
-    return result;
+  if (node.right !== null) {
+    result = result.concat(preOrder(node.right));
   }
-  
-  function inOrder(node) {
-    let result = [];
-  
-    if (node.left !== null) {
-      result = result.concat(inOrder(node.left));
-    }
-    result.push(node.value);
-    if (node.right !== null) {
-      result = result.concat(inOrder(node.right));
-    }
-  
-    return result;
+
+  return result;
+}
+
+function inOrder(node) {
+  let result = [];
+
+  if (node.left !== null) {
+    result = result.concat(inOrder(node.left));
   }
-  
-  function postOrder(node) {
-    let result = [];
-  
-    if (node.left !== null) {
-      result = result.concat(postOrder(node.left));
-    }
-  
-    if (node.right !== null) {
-      result = result.concat(postOrder(node.right));
-    }
-  
-    result.push(node.value);
-  
-    return result;
+  result.push(node.value);
+  if (node.right !== null) {
+    result = result.concat(inOrder(node.right));
   }
-  
-  const tree = new TreeNode(10);
-  
-  tree.addNode(3);
-  tree.addNode(6);
-  tree.addNode(1);
-  
-  tree.addNode(12);
-  tree.addNode(11);
-  tree.addNode(17);
-  console.log(preOrder(tree));
-  console.log(inOrder(tree));
-  
-  console.log(postOrder(tree));
-  
+
+  return result;
+}
+
+function postOrder(node) {
+  let result = [];
+
+  if (node.left !== null) {
+    result = result.concat(postOrder(node.left));
+  }
+
+  if (node.right !== null) {
+    result = result.concat(postOrder(node.right));
+  }
+
+  result.push(node.value);
+
+  return result;
+}
+
+function searchValue(tree,value){
+    if(tree === null){
+      return -1;
+    }
+
+    if(tree.value === value){
+      return tree.value;
+    }
+
+    if(tree.value > value){
+      return searchValue(tree.left,value);
+    }else if(tree.value < value){
+      return searchValue(tree.right,value);
+    }
+
+
+}
+
+const tree = new TreeNode(10);
+
+tree.addNode(3);
+tree.addNode(6);
+tree.addNode(1);
+
+tree.addNode(12);
+tree.addNode(11);
+tree.addNode(17);
+console.log(preOrder(tree));
+console.log(inOrder(tree));
+
+console.log(postOrder(tree));
+
+console.log(searchValue(tree,11));
